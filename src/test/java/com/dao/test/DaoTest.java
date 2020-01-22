ba6096b8334f5caf109fa8a591c16b8afa550ba4
Ggbr-TI <lt1289638486@163.com>
2020-01-22 22:13:03 +0800

    Origin

---
+++ src/test/java/com/dao/test/DaoTest.java
@@ -0,0 +1,15 @@
+package com.dao.test;
+
+import org.junit.Test;
+
+public class DaoTest {
+
+	@Test
+	public void test() {
+		/*ApplicationContext app=new ClassPathXmlApplicationContext("applicationContext.xml");
+		GoodsTypeMapper dao = app.getBean(GoodsTypeMapper.class);
+		Integer rs = dao.changeTypeState(1, 1);
+		System.out.println(rs);*/
+	}
+
+}
